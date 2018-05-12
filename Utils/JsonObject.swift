c137aa513ecad3fc3612cf7253f01ccb241f6f0d
Willian <w.policiano@gmail.com>
2018-05-12 01:07:35 -0300

    Enable code coverage and remove some unused files

---
+++ Utils/JsonObject.swift
@@ -0,0 +1,12 @@
+//
+//  JsonProvider.swift
+//  Utils
+//
+//  Created by Willian on 05/05/2018.
+//  Copyright © 2018 Willian Fagner Souza Polici. All rights reserved.
+//
+
+import Foundation
+
+public typealias JsonObject = [String : Any?]
+public typealias JsonArray = [JsonObject]
