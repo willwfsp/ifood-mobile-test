518d9e5ee428609350fa0917fb770dfa0960e695
Willian <w.policiano@gmail.com>
2018-05-15 01:44:13 -0300

    Classify the tweet sentiment

---
+++ Domain/Repositories/NaturalLanguageRepository.swift
@@ -0,0 +1,9 @@
+//
+//  NaturalLanguageRepository.swift
+//  Domain
+//
+//  Created by Willian on 14/05/2018.
+//  Copyright © 2018 Willian. All rights reserved.
+//
+
+import Foundation
---
+++ Store/DataSource/NaturalLanguageService.swift
@@ -0,0 +1,14 @@
+//
+//  SentimentService.swift
+//  Store
+//
+//  Created by Willian on 14/05/2018.
+//  Copyright © 2018 Willian. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum SentimentService {
+    case 
+}
